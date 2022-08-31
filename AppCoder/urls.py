7328c1b3127525aee730f8a4ef93a3013f761477
Gera Said <gerardo.said.86@gmail.com>
2022-08-31 20:12:29 -0300

    https://github.com/gsaid1986/MVT-Said.git

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,9 +4,9 @@ from AppCoder.views import *
 
 urlpatterns = [
     path('curso/', curso),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('estudiantes/', estudiantes),
-    path('profesores/', profesores),
-    path('entregables/', entregables),
+    path('', inicio, name= "inicio"),
+    path('cursos/', cursos, name= "cursos"),
+    path('estudiantes/', estudiantes, name= "estudiantes"),
+    path('profesores/', profesores, name="profesores"),
+    path('entregables/', entregables, name= "entregables"),
 ]
