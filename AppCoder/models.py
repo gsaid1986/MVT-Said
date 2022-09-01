6f7b8a4ea395dce9a8f272a483e73763e1fabe49
Gera Said <gerardo.said.86@gmail.com>
2022-09-01 11:10:02 -0300

    fin clase20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -16,6 +16,9 @@ class Estudiante(models.Model):
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.nombre+" "+self.apellido
+
 class Profesor(models.Model): 
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
