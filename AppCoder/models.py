3ecca03d2d7a353a2553c95d7f20c598e43c3a2d
Gera Said <gerardo.said.86@gmail.com>
2022-09-01 10:31:52 -0300

    clase20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -8,6 +8,9 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
 
+    def __str__(self):
+        return self.nombre+" "+str(self.comision)
+
 class Estudiante(models.Model): 
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
